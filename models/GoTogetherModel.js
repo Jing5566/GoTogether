de940bad0cdd215151af49a63a9904ca6b1f2217
Thomas <Thomas8241@gmail.com>
2023-09-12 17:12:35 -0500

    Changes made to the Schema worked, now only grabs the information that is applied and ignored fields that are left empty, old version of schema is in the Notes.md if needed

--- models/GoTogetherModel.js
+++ models/GoTogetherModel.js
@@ -8,70 +8,97 @@ const GoTogetherUserSchema = mongoose.Schema({
         // Workaround is to utilize cloudinary to store the images
         // use url link
         Profile_Image: {
-            type: String
+            type: String,
+            required: false,
+            set: a => a === '' ? undefined : a
         },
 
         First_Name: {
             type: String,
             maxLength: [20, "Must be less than 20 characters"],
-            required: [true, "First Name required"]
+            required: [true, "First Name required"],
+            set: b => b === '' ? undefined : b
         },
 
         Last_Name: {
             type: String,
             maxLength: [20, "Must be less than 20 characters"],
-            required: [true, "First Name required"]
+            required: [true, "First Name required"],
+            set: c => c === '' ? undefined : c
         },
 
         Birthday: {
             type: Date,
-            required: [true, "Birthdate must be entered."]
+            required: [true, "Birthdate must be entered."],
+            set: d => d === '' ? undefined : d
         },
 
         Age: {
-            type: Number
+            type: Number,
+            required: false,
+            set: e => e === '' ? undefined : e
         },
 
         Gender: {
-            type: String
+            type: String,
+            required: false,
+            set: f => f === '' ? undefined : f
         },
 
         Current_City: {
-            type: String
+            type: String,
+            required: false,
+            set: g => g === '' ? undefined : g
         },
 
         Hometown: {
-            type: String
+            type: String,
+            required: false,
+            set: h => h === '' ? undefined : h
         },
 
         Profession: {
-            type: String
+            type: String,
+            required: false,
+            set: i => i === '' ? undefined : i
         },
         
         Interests: {
-            type: String
+            type: String,
+            required: false,
+            set: j => j === '' ? undefined : j
         },
 
         // movie API to be used for this section?
         Favorite_Movie: {
-            type: String
+            type: String,
+            required: false,
+            set: k => k === '' ? undefined : k
         },
         
         Favorite_Artist: {
-            type: String
+            type: String,
+            required: false,
+            set: l => l === '' ? undefined : l
         },
 
         Favorite_Food: {
-            type: String
+            type: String,
+            required: false,
+            set: m => m === '' ? undefined : m
         },
 
         Languages: {
-            type: String
+            type: String,
+            required: false,
+            set: n => n === '' ? undefined : n
         },
 
         More_About_You: {
             type: String,
-            maxLength: [500, "Must be less than 500 characters"]
+            maxLength: [500, "Must be less than 500 characters"],
+            required: false,
+            set: o => o === '' ? undefined : o
         }
     }
 );
